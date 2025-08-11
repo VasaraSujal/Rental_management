80e14c3122f1d1feeb751b980036baad24fbb98c
Sujal Vasara <vasarasujal.cg@gmail.com>
2025-08-12 04:02:56 +0530

    add backend code for payment method

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,5 @@
 const app = require('./app');
 const { connectToMongoDB } = require('./config/db');
-// require("dotenv").config({ path: "./auth.env" });
 require("dotenv").config({ path: "./auth.env" });
 const PORT = process.env.PORT || 5500;
 

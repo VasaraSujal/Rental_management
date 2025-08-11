44bb3e26e46a08221d346f832f99e9edc04bee60
arya patel <aryaunjha@gmail.com>
2025-08-12 02:15:34 +0530

    set the delate option in cart

--- Backend/modules/products/productroutes.js
+++ Backend/modules/products/productroutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addproduct, producttransaction, getAllProducts,addincart, getcartproductbyemail,deleteFromCart } = require('./productcontrollers');
+const { addproduct, producttransaction, getAllProducts,addincart, getcartproductbyemail,deleteFromCart, deleteFromCart2 } = require('./productcontrollers');
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post('/addproduct', addproduct);
 router.post('/addincart', addincart);
 router.get('/cart/:email', getcartproductbyemail);
 router.delete('/cartdelete/:email/:productId', deleteFromCart);
+router.delete('/cart/:email', deleteFromCart2);
 
 router.post('/producttransaction', producttransaction);
 
